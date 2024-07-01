4e5496fb11f8ff53223f6dfda5f2f0e8e384afb5
Frank Iannelli <frank.iannelli@skykraft.com.au>
2024-07-01 13:51:43 +1000

    update

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -2,6 +2,7 @@ sudo apt install neovim
 
 if [ ! -d "$HOME/.config/nvim" ]; then
 	git clone https://github.com/LazyVim/starter ~/.config/nvim
+	git clone https://github.com/github/copilot.vim.git ~/.config/nvim/pack/github/start/copilot.vim
 	# Disable update notification popup in starter config
 	sed -i 's/checker = { enabled = true }/checker = { enabled = true, notify = false }/g' ~/.config/nvim/lua/config/lazy.lua
 	mkdir -p ~/.config/nvim/plugin/after
